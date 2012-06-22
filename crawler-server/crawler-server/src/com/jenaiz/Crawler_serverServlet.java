d2cbc72f2258eb1fe04bf1a3ca75f4e2645faa66
Jesús Navarrete <jesus.navarrete@gmail.com>
2012-06-22 18:05:25 +0200

    First commit of an App Engine application for Crawler Server

---
+++ crawler-server/crawler-server/src/com/jenaiz/Crawler_serverServlet.java
@@ -0,0 +1,11 @@
+package com.jenaiz;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Crawler_serverServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
